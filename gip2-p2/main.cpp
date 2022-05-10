89a867e37a4c62cf84845273c487e41aac7b06d8
Marco Schöttelkotte <marcoschoettelkotte@gmx.de>
2022-05-10 22:10:13 +0200

    Initial Commit

---
+++ gip2-p2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gip2-p2
+//
+//  Created by Marco Schöttelkotte on 10.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
